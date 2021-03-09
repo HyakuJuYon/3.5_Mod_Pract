52f8761782d2a52cb7e8bc513337a0a372381a91
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-09 20:32:40 +0300

    Changes_1

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace _3._5_Mod_Pract
     {
         static void Main(string[] args)
         {
-            string MyName = "Jane";
+            string name = "Jane";
 
             var age = 27;
 
             var favcolor = "black";
 
-            Console.WriteLine("{0} \n {1} \n {2}", MyName, age, favcolor);
+            Console.WriteLine("{0} \n {1} \n {2}", name, age, favcolor);
 
             Console.ReadKey();
         }
