fc761d5a03d56e0a7e3c1fb83409e1a0d0a7e07f
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-09 20:31:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3._5_Mod_Pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Jane";
+
+            var age = 27;
+
+            var favcolor = "black";
+
+            Console.WriteLine("{0} \n {1} \n {2}", MyName, age, favcolor);
+
+            Console.ReadKey();
+        }
+    }
+}
